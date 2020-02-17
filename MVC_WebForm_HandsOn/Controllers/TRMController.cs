175677fa1c2258ad618e2d27eab1aca858cdf104
TejalThorat55 <60806918+TejalThorat55@users.noreply.github.com>
2020-02-17 13:39:13 +0530

    MVC_WebForm_HandsOn

---
+++ MVC_WebForm_HandsOn/Controllers/TRMController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using MVC_Webform_HandsOn;
+
+namespace MVC_Webform_HandsOn.Controllers
+{
+    public class TRMController : Controller
+    {
+        // GET: TRM
+        [HttpPost]
+        public ActionResult Index(string re, string sk, DateTime st, DateTime en)
+        {
+            PIYAEntities ctx = new PIYAEntities();
+            New_Training tr = new New_Training();
+
+            tr.Req_Name = re;
+            tr.Skill = sk;
+            tr.Start_D = st;
+            tr.End_D = en;
+            ctx.New_Training.Add(tr);
+            ctx.SaveChanges();
+           
+
+            return View();
+        }
+    
+
+    
+    public ActionResult Index()
+    {
+        return View();
+    }
+        }
+}
---
+++ MVC_WebForm_HandsOn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Webform_HandsOn.MvcApplication" Language="C#" %>
