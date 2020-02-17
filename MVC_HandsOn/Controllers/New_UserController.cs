fa63468ac41c5c6c469d71fe856fa4630b4c4490
TejalThorat55 <60806918+TejalThorat55@users.noreply.github.com>
2020-02-17 13:37:15 +0530

    MVC_HandsOn

---
+++ MVC_HandsOn/Controllers/New_UserController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MVC_HandsOn.Controllers
+{
+    public class New_UserController : Controller
+    {
+        // GET: New_User
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        
+        [HttpPost]
+        public ActionResult Index(string UN, string pwd)
+        {
+            HANDSONEntities1 ctx = new HANDSONEntities1();
+
+            LoginInfo a = new LoginInfo();
+            UserDetail b = new UserDetail();
+
+            
+            var rec = ctx.LoginInfoes.Where(d => d.UserName == UN && d.Passwrd == pwd).FirstOrDefault();
+            if(rec!=null)
+            {
+                /* */
+                return Redirect("/New_User/Output?ID="+rec.UserId);
+            }
+            ViewBag.Msg = "Error in SignIn";
+            return View();
+        }
+
+      
+
+        public ActionResult Output()
+        {
+
+            int id = int.Parse(Request["ID"]);
+            
+           
+
+            HANDSONEntities1 ctx = new HANDSONEntities1();
+
+            var obj = ctx.UserDetails.Where(u => u.UserId == id).FirstOrDefault();
+            if (obj != null)
+            {
+                ViewBag.FullNme = obj.FullName;
+                ViewBag.age = obj.Age;
+                ViewBag.city = obj.City;
+                ViewBag.rep = obj.ReportingTo;
+            }
+            return View();
+        }
+    }
+}
---
+++ MVC_HandsOn/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_HandsOn
+{
+    public class UserInfo
+    {
+        public string UN { get; set; }
+        public string pwd { get; set; }
+    }
+}
