c41df6bd3f265cb0c609cedb5e0d368409bcebeb
TejalThorat55 <60806918+TejalThorat55@users.noreply.github.com>
2020-02-19 09:27:43 +0530

    MVC_Mail_HandsOn

---
+++ MVCMail_HandsOn/Controllers/MailController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Data;
+using System.Data.SqlClient;
+using MVC_MailHandsOn.Models;
+
+namespace MVC_MailHandsOn.Controllers
+{
+    public class MailController : Controller
+    {
+        // GET: Mail
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string un, string pwd)
+        {
+            TejalEntities1 ctx = new TejalEntities1();
+
+            var res= ctx.UserInfoes.Where(n=>n.UserName==un && n.Passwrd==pwd).FirstOrDefault();
+
+            if(res!=null)
+            {
+                return Redirect("/Mail/Process?ID=" + res.UserId);
+            }
+            ViewBag.msg = "UserId and Password Doesn't Match";
+            return View();
+        }
+
+       
+        public ActionResult Process()
+        {
+            int id = int.Parse(Request["ID"]);
+
+            MVCMail model = new MVCMail();
+
+            TejalEntities1 ctx = new TejalEntities1();
+
+            var obj = ctx.MailDetails.Where(u => u.UserId == id);
+
+            foreach(var a in obj)
+            {
+                
+
+                ViewBag.mailid = a.MailId;
+                ViewBag.mailfrom = a.MailFrom;
+                ViewBag.sub = a.Sub;
+                ViewBag.receivedate = a.ReceiveDate;
+                ViewBag.msg = a.Msg;
+
+               
+            }
+            return View();
+        }
+    }
+}
---
+++ MVCMail_HandsOn/Models/MVCMail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_MailHandsOn.Models
+{
+    public class MVCMail
+    { 
+        public int mailid;
+        public string mailfrom;
+        public string msg;
+        public DateTime receivedate;
+        public string sub;
+    }
+}
